f1a5386a592842796063cc4b6d16aa52e081b5d8
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-08-10 20:32:32 +0200

    Improve examples

--- Examples/Example-SendToVirusTotalFile.ps1
+++ Examples/Example-SendToVirusTotalFile.ps1
@@ -6,6 +6,8 @@ $VTApi = Get-Content -LiteralPath "C:\Support\Important\VirusTotalApi.txt"
 $Output = New-VirusScan -ApiKey $VTApi -File "$PSScriptRoot\Submisions\TestFile.txt"
 $Output | Format-List
 
+Start-Sleep -Seconds 120
+
 # Since the output will return scan ID we can use it to get the report
 $OutputScan = Get-VirusReport -ApiKey $VTApi -AnalysisId $Output.data.id
 $OutputScan | Format-List
---
+++ Examples/Example-SendToVirusTotalRescan.ps1
@@ -0,0 +1,11 @@
+﻿Import-Module .\VirusTotalAnalyzer.psd1 -Force
+
+$VTApi = Get-Content -LiteralPath "C:\Support\Important\VirusTotalApi.txt"
+
+# Submit file hash to rescan from existing file (doesn't sends the file)
+$Output = New-VirusScan -ApiKey $VTApi -FileHash "$PSScriptRoot\Submisions\TestFile.txt"
+$Output | Format-List
+
+# Submit hash to rescan
+$Output = New-VirusScan -ApiKey $VTApi -Hash "ThisHashHasToExistsOnVirusTotal"
+$Output | Format-List
